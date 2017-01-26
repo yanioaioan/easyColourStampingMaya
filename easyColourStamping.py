c335d734c3dddd2eb4d9a7248c806c516a4cb70e
Yannis Ioannidis <yanioaioan@hotmail.com>
2017-01-26 11:56:10 +0000

    fixed

--- easyColourStamping.py
+++ easyColourStamping.py
@@ -33,37 +33,66 @@ def getVertexColor():
     
     
 # this simple function is called each time the script below decides an object should be located
-def locate_object(x,z):
+def locate_object(x,y,z):
 			cmds.polyCone()		
 			cmds.scale(0.5, 0.5, 0.5)
-			cmds.move( x-50.0, 0.0, z-50.0)
-    
-    
+			cmds.move( x,y,z)
+
+
+
+def apply_texture(object):
+        
+        cmds.sets(name='imageMaterialGroup', renderable=True, empty=True)
+        shaderNode = cmds.shadingNode('phong', name='shaderNode', asShader=True)
+        fileNode = cmds.shadingNode('file', name='fileTexture', asTexture=True)
+        myfile = ("textures"+ "/" +"1637192.jpg")
+        cmds.setAttr('fileTexture'+'.fileTextureName', myfile, type="string")
+        shadingGroup = cmds.sets(name='textureMaterialGroup', renderable=True, empty=True)
+        cmds.connectAttr('shaderNode'+'.outColor','textureMaterialGroup'+'.surfaceShader', force=True)
+        cmds.connectAttr('fileTexture'+'.outColor','shaderNode'+'.color', force=True)
+        cmds.surfaceShaderList('shaderNode', add='imageMaterialGroup')
+        cmds.sets(object, e=True, forceElement='imageMaterialGroup')
+
+
+cmds.file(new=True, force=True)
+
+
+#create a sphere    
 mysphereradius=5
 mysphere=cmds.polySphere(r=sphereradius)
+
 #now assign a texture to sphere
+#create one shader		
+myshadingNodeName1=createShaderwithText(1)
+cmds.select(mysphere[0])
+apply_texture(mysphere[0])
+cmds.DisplayShadedAndTextured()
+
 
 
 #save a list of all shapes world positions
-sphereVertexList=getVtxPos('pSphereShape1')
-size=len(sphereVertexList)
+sphereVertexPosList=getVtxPos('pSphereShape1')
+size=len(sphereVertexPosList)
 vertexCount=0
 #loop and print all positions of the vertices on the sphere
 for vertexnumber in range(size):
 	
-	cmds.select(mysphere[0]+'.vtx['+str(vertexnumber)+']', r=True)
-	
+	#cmds.select(mysphere[0]+'.vtx['+str(vertexnumber)+']', r=True)	
 	#####Works just fine keep on working on it!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	#vertexnumber=203
-	cmds.select(mysphere[0]+'.vtx['+str(vertexnumber)+']')
+	#cmds.select(mysphere[0]+'.vtx['+str(vertexnumber)+']')
+	
 	r= cmds.polyListComponentConversion( mysphere[0]+'.vtx['+str(vertexnumber)+']', fv=True, tuv=True, internal=True )
-	cmds.select(r)
+	#cmds.select(r)
 	uvs=cmds.polyEditUV(r, query=True )
-	sample = cmds.colorAtPoint( 'file1', output ='RGBA', u =uvs[0], v = uvs[1] )
-	print sample
-	if sample[0] > 0.7 and sample[1] > 0.2 and sample[2] > 0.2: # checks if the red component is more than 0.7 and the other to channels have significantly smaller values
-		locate_object(x,z)	
-		
-
-
-  
+	sample = cmds.colorAtPoint( 'fileTexture.fileTextureName', output ='RGBA', u =uvs[0], v = uvs[1] )
+	
+	if sample[0] > 0.9 and sample[1] < 0.2 and sample[2] < 0.2: # checks if the red component is more than 0.7 and the other to channels have significantly smaller values
+		print "sample=%s"%sample
+		print "vertexnumber=%s"%vertexnumber
+		#cmds.select(mysphere[0]+'.vtx['+str(vertexnumber)+']')
+		locate_object(sphereVertexPosList[vertexnumber][0], sphereVertexPosList[vertexnumber][1], sphereVertexPosList[vertexnumber][2])	
+				
+	
+	
+	  
